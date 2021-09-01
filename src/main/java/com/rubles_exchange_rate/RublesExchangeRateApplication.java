e462a16cd2bc471340f101c7250cbdb5f3795dc1
vladocrat <vladocrat@gmail.com>
2021-09-01 12:25:47 +0300

    Initial commit

---
+++ src/main/java/com/rubles_exchange_rate/RublesExchangeRateApplication.java
@@ -0,0 +1,13 @@
+package com.rubles_exchange_rate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RublesExchangeRateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RublesExchangeRateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rubles_exchange_rate/RublesExchangeRateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rubles_exchange_rate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RublesExchangeRateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
